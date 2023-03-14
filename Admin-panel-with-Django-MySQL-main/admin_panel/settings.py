836861598b19fa773af65c0036d4fe3eed3639ed
AdityaSharma-23 <aditya.sharma.23082002@gmail.com>
2023-03-14 11:58:41 +0530

    Updated

--- Admin-panel-with-Django-MySQL-main/admin_panel/settings.py
+++ Admin-panel-with-Django-MySQL-main/admin_panel/settings.py
@@ -81,10 +81,10 @@ DATABASES = {
      'default': {
          'ENGINE': 'django.db.backends.mysql',
          'NAME': 'test1',
-         'HOST': '127.0.0.1',
+         'HOST': 'localhost',
          'PORT': '3306',
          'USER': 'root',
-         'PASSWORD': 'Oikawa.sql@1',
+         'PASSWORD': 'root',
      }
 }
 
