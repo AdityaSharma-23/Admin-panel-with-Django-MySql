726980db35436b48869a7989dc6a0094e55c5f8e
Aditya Sharma <aditya.sharma.23082002@gmail.com>
2023-03-20 13:58:07 +0530

    Update settings.py

--- Admin-panel-with-Django-MySQL-main/admin_panel/settings.py
+++ Admin-panel-with-Django-MySQL-main/admin_panel/settings.py
@@ -132,6 +132,5 @@ STATICFILES_DIRS = [
 ]
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
